4757a9785bb6131d1a3db60c8e4a99d1f9b56796
acstanie <alexhartman95@gmail.com>
2025-06-28 14:01:34 -0400

    Add read button functionality

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-const myLibrary = [{
-    title: 'hello',
-    author: 'carl',
-    pages: 23,
-    condition: 'yes',
-}];
-
-function Book(title, author, pages, condition) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.condition = condition;
-}
-
-Book.prototype.change = function(btn) {
-    btn.addEventListener('click', (e) => {
-    if(this.condition === 'Read') {
-        this.condition = 'Not Read';
-        btn.textContent = 'Not Read';
-    } else if(this.condition === 'Not Read') {
-        this.condition === 'Read';
-        btn.textContent = 'Read';
-    }
-    });
-}
-
 const addNewBook = document.querySelector('.add-new-book');
 const dialog = document.querySelector('dialog');
 const close = document.querySelector('.close-btn');
 const addBook = document.querySelector('.add-book');
-
 
 const form = document.querySelector('form');
 const title = document.querySelector('#title');
@@ -38,7 +11,99 @@ const condition = document.querySelector('select');
 
 const results = document.querySelector('.results');
 
+const myLibrary = [{
+    id: crypto.randomUUID(),
+    title: 'hello',
+    author: 'carl',
+    pages: 23,
+    condition: 'Read',
+}];
 
+function Book(id, title, author, pages, condition) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.condition = condition;
+}
+
+Book.prototype.change = function(btn) {
+    btn.addEventListener('click', (e) => {
+        if(this.condition === 'Read') {
+            this.condition = 'Not-Read';
+            btn.textContent = 'Not-Read';
+        } 
+        if(this.condition === 'Not-Read') {
+            this.condition === 'Read';
+            btn.textContent = 'Read';
+        }
+    });
+}
+
+function display(array) {
+    for(let i = 0; i < array.length; i++) {
+        const div = document.createElement('div');
+        div.classList.toggle("books");
+                
+        div.innerHTML = `  
+                        <span class="book-id">${array[i].id}</span><br> 
+                        Title: ${array[i].title}<br>
+                        Author: ${array[i].author}<br>
+                        Pages: ${array[i].pages}<br>
+                        <span id='stat'>Status: ${array[i].condition}</span><br>`
+        
+        const btn1 = document.createElement('button');
+        btn1.classList.toggle('del-btn');
+        btn1.textContent = 'Delete';
+        div.appendChild(btn1);
+
+        const btn2 = document.createElement('button');
+        btn2.classList.toggle('change-btn');
+        if (array[i].condition === "Not-Read") {
+            btn2.textContent = "Read"; 
+        }
+        if (array[i].condition === "Read") {
+            btn2.textContent = "Not-Read"; 
+        }
+        div.appendChild(btn2);
+
+        results.appendChild(div);    
+    }
+}
+
+function remove(array) {
+    results.addEventListener("click", (e) => {
+        if (e.target.matches(".del-btn")) {
+            const id = e.target.parentNode.querySelector(".book-id").textContent;
+            
+            array.forEach(book => {
+                if (book.id === id) {
+                    let position = array.indexOf(book);
+                    array.splice(position, 1);
+                }
+            });
+
+            e.target.parentNode.remove();
+        }
+    });
+}
+
+results.addEventListener("click", (e) => {
+    if(e.target.matches(".change-btn")) {
+        const stat = e.target.parentNode.querySelector("#stat");
+        if (stat.textContent === "Status: Not-Read") {
+            console.log(true);
+            stat.innerHTML = "Status: Read";
+            e.target.textContent = "Not-Read";
+            return;
+        }
+        if (stat.textContent === "Status: Read") {
+            stat.innerHTML = "Status: Not-Read";
+            e.target.textContent = "Read";
+            return;
+        }
+    }
+});
 
 addNewBook.addEventListener('click', (e) => {
     dialog.showModal();
@@ -51,49 +116,16 @@ close.addEventListener('click', (e) => {
 addBook.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const book = new Book(title.value, author.value, pages.value, condition.value);
+    let id = crypto.randomUUID()
+    const book = new Book(id, title.value, author.value, pages.value, condition.value);
     myLibrary.push(book);
+    results.innerHTML = "";
     display(myLibrary);
+    remove(myLibrary);
 
     const btn2 = document.querySelector('.change-btn');
     book.change(btn2);
     
     form.reset();
+    dialog.close();
 });
-
-function display(array) {
-    for(let i = 0; i < array.length; i++) {
-        const div = document.createElement('div');
-                
-        div.innerHTML = `Title: ${array[i].title}<br>
-                            Author: ${array[i].author}<br>
-                            Pages: ${array[i].pages}<br>
-                            Status: ${array[i].condition}<br>`
-        
-        const btn1 = document.createElement('button');
-        btn1.classList.toggle('del-btn');
-        btn1.textContent = 'Delete';
-        div.appendChild(btn1);
-
-        const btn2 = document.createElement('button');
-        btn2.classList.toggle('change-btn');
-        btn2.textContent = 'Read';
-        div.appendChild(btn2);
-    
-        results.appendChild(div);      
-    }
-
-    remove();
-}
-
-function remove() {
-    const del = document.querySelectorAll('.del-btn');
-
-    for(let i = 0; i < del.length; i++) {
-        del[i].addEventListener('click', (e) => {
-            del[i].parentNode.remove();
-        });
-    }
-}
-
-console.log(Object.getPrototypeOf(book))
